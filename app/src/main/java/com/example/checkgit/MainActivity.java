0992fb01fc7cc15fe8667967993e78f0b6f760ac
arzooGithub <52627099+arzooGithub@users.noreply.github.com>
2020-09-16 00:03:49 -0400

    again

--- app/src/main/java/com/example/checkgit/MainActivity.java
+++ app/src/main/java/com/example/checkgit/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /* here i commit some change */
+        /* here i commit againchange */
         
     }
 }
